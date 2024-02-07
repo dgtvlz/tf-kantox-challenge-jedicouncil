28769a289bbbb235c6e9dc9adde664154eda4490
dgtvlz <diegogarcia@netrixllc.com>
2024-02-07 12:28:05 -0300

    renaming resource

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,7 +1,7 @@
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "${var.env.prefix}-${var.env.project}-dynamodb"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "jedi_id"
+resource "aws_dynamodb_table" "locations" {
+  name         = "${var.env.prefix}-${var.env.project}-dynamodb"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "jedi_id"
 
   attribute {
     name = "jedi_id"
